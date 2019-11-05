45b8765885f662acc28de5a9e105b18d5c0a468d
PhyllisNabangi <nabangiphyllis7@gmail.com>
2019-11-05 10:00:38 +0300

    Added the GET request

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ app.listen(3000, function() {
     console.log('listening on 3000');
     })
 
-//Sending a get request
+//Sending a GET request
+app.get('/', function(req, res){
+    res.send('Hello World')
+})
