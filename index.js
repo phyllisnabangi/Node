4e6816958ecfd34b76b6649eb29a1e3df272e9e2
PhyllisNabangi <nabangiphyllis7@gmail.com>
2019-11-06 08:36:36 +0300

    changed to ES6 arrow function in index.js file

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 const express =  require('express');
 const app = express()
 
-app.listen(3000, function() {
+app.listen(3000, () => {
     console.log('listening on 3000');
     })
 
 //Sending a GET request
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.send('Hello World')
 })
