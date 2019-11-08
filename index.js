5c5e3667705de82eb25bd56164860b1470399d9d
PhyllisNabangi <nabangiphyllis7@gmail.com>
2019-11-08 22:58:47 +0300

    Added Layout and linked the pages

--- index.js
+++ index.js
@@ -25,17 +25,25 @@ app.post('/reg', (req, res) => {
 })
 
 //Thanks Route
-app.post('/thanks', (req, res) => {
-    res.send("Hello " + req.body.firstname);
-    //  res.render('index', {
-    //     name: req.body.firstname
-    // });
+app.post('/submit', (req, res) => {
+    // res.send("Hello " + req.body.firstname);
+     res.render('form_data', {
+        fname: req.body.firstname,
+        lname: req.body.lastname,
+        email: req.body.emailaddress,
+        gender: req.body.gender,
+        country: req.body.country,
+        city: req.body.city,
+        password: req.body.password
+    });
 })
 
 app.get('/index', (req, res) => {
     res.render('index');
 })
-//end of pug
+app.get('/data', (req, res) => {
+    res.render('form_data');
+})
 
 
 app.listen(3000, () => {
