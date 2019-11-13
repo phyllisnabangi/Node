9266da6d515a473795736b4d88d0cb223cf2d574
PhyllisNabangi <nabangiphyllis7@gmail.com>
2019-11-13 09:51:07 +0300

    Refactored code

--- index.js
+++ index.js
@@ -1,136 +1,64 @@
-const path = require('path')
-const bodyParser = require('body-parser')
-// creates app by calling express
+// Requiring the packages
 const express =  require('express');
-const app = express()
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
-// body-parser to parse request body data
-app.use(bodyParser.json());
+
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-//Setting the view engine to pug
-app.set('view engine', 'pug');
-
-//Setting the folder
-app.set('views', path.join(__dirname, 'views'))
-
-//Setting the server to port 3000
-app.listen(3000, () => {
-    console.log('listening on 3000');
-    })
-
-// Mongoose connects the app to the mongoDB
-var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
+// mongoose db connection
 mongoose.connect("mongodb://localhost:27017/node-demo");
-  
-// Model
 
-// Creating a DB Schema
-var nameSchema = new mongoose.Schema({
-    firstname: String,
-    lastname: String
-  });
-
-// Creating a model
+// schema definition
+const nameSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: 'Please Enter first name'
+               },
+    lastname: String,
+    emailaddress: String,
+    gender: String,
+    city: String,  
+});
+// Creating an instance of the model
 var User = mongoose.model("User", nameSchema)
 
-// RESTFUL API - mechanism by which data will be added to the database
+// Routes
+app.get('/reg', (req, res) => {
+    res.render('form');
+})
 
-// CRUD endpoint
-app.post("/regData", (req, res) => {
-    // saving data to the db
-    var myData = new User(req.body);
+app.post('/reg', (req, res) => {
+    const myData= new User(req.body)
     myData.save()
-        // A promise-start --what is returned when the save to the database completes
         .then(item => {
-            res.send("item saved to database");
+            User.find().then(
+                items => {
+                    res.render('list', {users:items})
+                })           
         })
         .catch(err => {
             res.status(400).send("unable to save to database");
         });
-        // Promise-end
-});
+ });
 
-
-
-
-
-
-
-
-//GET Route
-app.get('/reg', (req, res, next) => {
-    res.render('form');
-})
-//POST Route
-app.post('/reg', (req, res) => {
-    // console.log("Form has been posted");
+//  Listening for requests: the server
+app.listen(3000, ()=>{
+    console.log("listening on 3000");
     
-    console.log('body', req.body);
-    console.log('Query Params', req.query);
-    // res.send(req.body)   
-})
-
-//Submit Route
-app.post('/submit', (req, res) => {
-    // res.send("Hello " + req.body.firstname);
-     res.render('form_data', {
-        fname: req.body.firstname,
-        lname: req.body.lastname,
-        email: req.body.emailaddress,
-        gender: req.body.gender,
-        country: req.body.country,
-        city: req.body.city,
-        password: req.body.password
-    });
-})
-
-app.get('/index', (req, res) => {
-    res.render('index');
-})
-app.get('/data', (req, res) => {
-    res.render('form_data');
 })
 
 
 
-//Sending a GET request
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
-
-app.get('/about', (req, res) => {
-    res.send('This is the about us page')
-})
-
-//POST Request
-app.post('/', (req, res) => {
-    res.send('Got a POST request')
-})
-
-//PUT Method
-app.put('/user', (req, res) => {
-    res.send('Got a PUT request at /user')
-})
-
-//DELETE Method
-app.delete('/user', (req, res) => {
-    res.send('Got a DELETE request at /user')
-})
-
-//Path Params - set them at the path
-app.get('/users/:name', (req, res) => {
-    res.send('Hello ' + req.params.name)
-})
-//Query Params - set them at the url
-app.get('/param', (req, res) =>{
-    res.send('This is a class ' + req.query.class + ' cohort ' + req.query.cohort)
-})
 
 
-//Error Page
-app.get('*', (req, res) => { res.send('Page not found');});
+
+
+
 
 
 
